92290b15829587911b9dabf644be849cf2b2dcd6
Brandon <brandontramos@gmail.com>
2019-02-01 17:59:56 -0500

    lexer and parser flags '-l','-p'

--- dumbledore.go
+++ dumbledore.go
@@ -9,9 +9,10 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 1 {
+	switch len(os.Args) {
+	case 1:
 		repl.Run()
-	} else {
+	case 2:
 		lex := lexer.NewLexerFromFile(os.Args[1])
 		p := parser.New(lex)
 		program := p.ParseProgram()
@@ -23,6 +24,27 @@ func main() {
 		}
 		for _, statement := range program.Statements {
 			fmt.Println(statement.ToString())
+		}
+	case 3:
+		flag := os.Args[2]
+		switch flag {
+		case "-l":
+			lex := lexer.NewLexerFromFile(os.Args[1])
+			lex.ReadTokens()
+		case "-p":
+			lex := lexer.NewLexerFromFile(os.Args[1])
+			p := parser.New(lex)
+			program := p.ParseProgram()
+			if len(p.Errors) > 0 {
+				for _, err := range p.Errors {
+					fmt.Println(err)
+				}
+				os.Exit(1)
+			}
+			for _, statement := range program.Statements {
+				fmt.Println(statement.ToString())
+			}
+
 		}
 
 	}
